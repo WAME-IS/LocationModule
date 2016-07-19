753525bd1ad0ca45b316ace5fea871571cf0fb52
Linzee <d.gmiterko@gmail.com>
2016-07-19 10:28:44 +0200

    IMPROVE positions

--- vendor/wame/SettingsModule/components/ContinentList/ContinentListControl.php
+++ vendor/wame/SettingsModule/components/ContinentList/ContinentListControl.php
@@ -1,37 +1,36 @@
-<?php 
+<?php
 
 namespace Wame\LocationModule\Vendor\Wame\SettingsModule\Components;
 
+use Nette\DI\Container;
 use Wame\Core\Components\BaseControl;
 use Wame\LocationModule\Repositories\ContinentRepository;
 
-
 interface IContinentListControlFactory
 {
-	/** @return ContinentListControl */
-	public function create();	
-}
 
+    /** @return ContinentListControl */
+    public function create();
+}
 
 class ContinentListControl extends BaseControl
 {
-	/** @var ContinentRepository */
-	private $continentRepository;
-	
-	
-	public function __construct(
-		ContinentRepository $continentRepository
-	) {
-		parent::__construct();
-		
-		$this->continentRepository = $continentRepository;
-	}
-	
-	
-	public function render()
-	{
-		$this->template->continents = $this->continentRepository->find(['status !=' => ContinentRepository::STATUS_REMOVED]);
-		$this->componentRender();
-	}
 
+    /** @var ContinentRepository */
+    private $continentRepository;
+
+    public function __construct(
+    Container $container, ContinentRepository $continentRepository
+    )
+    {
+        parent::__construct($container);
+
+        $this->continentRepository = $continentRepository;
+    }
+
+    public function render()
+    {
+        $this->template->continents = $this->continentRepository->find(['status !=' => ContinentRepository::STATUS_REMOVED]);
+        $this->componentRender();
+    }
 }
--- vendor/wame/SettingsModule/components/StateList/StateListControl.php
+++ vendor/wame/SettingsModule/components/StateList/StateListControl.php
@@ -1,54 +1,51 @@
-<?php 
+<?php
 
 namespace Wame\LocationModule\Vendor\Wame\SettingsModule\Components;
 
+use Nette\DI\Container;
 use Wame\Core\Components\BaseControl;
 use Wame\LocationModule\Repositories\StateRepository;
 use Wame\LocationModule\Vendor\Wame\SettingsModule\Forms\StateListForm;
 
-
 interface IStateListControlFactory
 {
-	/** @return StateListControl */
-	public function create();	
-}
 
+    /** @return StateListControl */
+    public function create();
+}
 
 class StateListControl extends BaseControl
 {
-	/** @var StateRepository */
-	private $stateRepository;
-	
-	/** @var StateListForm */
-	public $stateListForm;
-	
-	/** @var array */
-	private $states = [];	
-	
-	
-	public function __construct(
-		StateRepository $stateRepository,
-		StateListForm $stateListForm
-	) {
-		parent::__construct();
-		
-		$this->stateRepository = $stateRepository;
-		$this->stateListForm = $stateListForm;
-	}
-	
-	
-	protected function createComponentStateListForm()
-	{
-		$form = $this->stateListForm->build();
-		
-		return $form;
-	}
-	
-	
-	public function render()
-	{
-		$this->template->states = $this->stateRepository->find(['status !=' => StateRepository::STATUS_REMOVE]);
-		$this->componentRender();
-	}
 
+    /** @var StateRepository */
+    private $stateRepository;
+
+    /** @var StateListForm */
+    public $stateListForm;
+
+    /** @var array */
+    private $states = [];
+
+    public function __construct(
+    Container $container, StateRepository $stateRepository, StateListForm $stateListForm
+    )
+    {
+        parent::__construct($container);
+
+        $this->stateRepository = $stateRepository;
+        $this->stateListForm = $stateListForm;
+    }
+
+    protected function createComponentStateListForm()
+    {
+        $form = $this->stateListForm->build();
+
+        return $form;
+    }
+
+    public function render()
+    {
+        $this->template->states = $this->stateRepository->find(['status !=' => StateRepository::STATUS_REMOVE]);
+        $this->componentRender();
+    }
 }
