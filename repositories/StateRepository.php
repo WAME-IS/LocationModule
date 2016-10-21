3ca5bca0097b9edb43f6371530e6f717916c9e01
Stanislav Červeňák <stano@wame.sk>
2016-10-21 16:32:15 +0200

    EDIT StateRepository

--- repositories/StateRepository.php
+++ repositories/StateRepository.php
@@ -2,10 +2,12 @@
 
 namespace Wame\LocationModule\Repositories;
 
+use Nette\Utils\Strings;
 use Wame\Core\Exception\RepositoryException;
 use Wame\LanguageModule\Repositories\TranslatableRepository;
 use Wame\LocationModule\Entities\StateEntity;
 use Wame\LocationModule\Entities\StateLangEntity;
+
 
 class StateRepository extends TranslatableRepository
 {
@@ -13,15 +15,16 @@ class StateRepository extends TranslatableRepository
     const STATUS_ENABLED = 1;
     const STATUS_DISABLED = 2;
 
-    
+
     public function __construct()
     {
         parent::__construct(StateEntity::class, StateLangEntity::class);
     }
 
+
     /**
      * Create state
-     * 
+     *
      * @param StateEntity $stateEntity
      * @return StateEntity
      * @throws RepositoryException
@@ -34,9 +37,10 @@ class StateRepository extends TranslatableRepository
         return $stateEntity;
     }
 
+
     /**
      * Update state
-     * 
+     *
      * @param StateEntity $stateEntity
      * @return StateEntity
      */
@@ -45,9 +49,10 @@ class StateRepository extends TranslatableRepository
         return $stateEntity;
     }
 
+
     /**
      * Change state status
-     * 
+     *
      * @param array $criteria
      * @param int $status
      */
@@ -56,24 +61,34 @@ class StateRepository extends TranslatableRepository
         $entity = $this->get($criteria);
         $entity->status = $status;
     }
+    
 
     /**
      * Return list of states in actual language
-     * 
+     *
      * @param array $criteria
+     * @param array $withoutIds
      * @return array
      */
-    public function getStateList($criteria = [])
+    public function getStateList($criteria = [], $withoutIds = [])
     {
         $return = [];
+
+        if (!is_array($withoutIds)) {
+            $withoutIds = [$withoutIds];
+        }
+
+        if (count($withoutIds) > 0) {
+            $criteria['id NOT IN'] = $withoutIds;
+        }
 
         $states = $this->find($criteria);
 
         foreach ($states as $state) {
-            $return[$state->getId()] = $state->langs[$this->lang]->getTitle();
+            $return[$state->getId()] = Strings::upper($state->getCode()) . ' - ' . $state->getTitle();
         }
 
         return $return;
     }
-    
+
 }
